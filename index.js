4f213c7507dae1c09b99a977d32c59ce6dfeba24
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2025-07-05 17:18:36 +0600

    full setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const config = {
+  trailingComma: "es5",
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  useTabs: true,
+  quoteProps: "consistent",
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  objectWrap: "collapse",
+  bracketSameLine: false,
+  arrowParens: "always",
+  requirePragma: false,
+  insertPragma: false,
+  checkIgnorePragma: false,
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
+  singleAttributePerLine: false,
+};
+
+export default config;
